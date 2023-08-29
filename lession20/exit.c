351887865f46583c4b5eb286ca55f71ec042f57e
pjb1998 <494654334@qq.com>
2023-08-29 16:13:59 +0800

    三种进程复习

--- lession20/exit.c
+++ lession20/exit.c
@@ -1,3 +1,12 @@
+/*
+    #include <stdlib.h>
+    void exit(int status);
+
+    #include <unistd.h>
+    void _exit(int status);
+
+    status参数：是进程退出时的一个状态信息。父进程回收子进程资源的时候可以获取到。
+*/
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -5,7 +14,8 @@
 int main(){
     printf("hello\n");
     printf("world");
-    //exit(0);
-    _exit(0);
-    return 0;
+    // exit(0);     //刷新I/O缓冲 
+    _exit(0);       //不刷新I/O缓冲
+    printf("after exit\r\n");
+    return 0;   //这里的return和exit都表示进程退出
 }
--- lession20/orphan.c
+++ lession20/orphan.c
@@ -41,7 +41,8 @@ int main() {
     int num = 10;
     /*
     具体来说，在fork()函数被调用后，以下情况发生：
-    1.在父进程中，fork()函数返回子进程的进程ID（PID），该值大于0。这样父进程就可以通过判断返回值来确定自己是父进程。
+    1.在父进程中，fork()函数返回子进程的进程ID（PID），该值大于0。
+    这样父进程就可以通过判断返回值来确定自己是父进程。
     2.在子进程中，fork()函数返回0。通过判断返回值为0，子进程可以确定自己是子进程。
     3.如果fork()函数调用失败，返回值为-1，表示创建子进程失败，这通常发生在系统资源不足的情况下。
     */
@@ -51,34 +52,34 @@ int main() {
 
     // 判断是父进程还是子进程
     if(pid > 0) {
-        
         // printf("pid : %d\n", pid);
-        // 如果大于0，返回的是创建的子进程的进程号，当前是父进程
-        printf("i am parent process, pid : %d, ppid : %d\n", getpid(), getppid());
-
+        // 如果大于0，fork()返回的是创建的子进程的进程号，当前是父进程
+        // 父进程执行中->切换到后台
+        // 父进程执行结束->切换到前台panjinbao@pan-Ubuntu:~/Linux/lession20$,但此时子进程还在执行所以产生
+        // panjinbao@pan-Ubuntu:~/Linux/lession20$ I am child process, pid : 3730, ppid : 1
+        printf("I am parent process, pid : %d, ppid : %d\n", getpid(), getppid());
     } else if(pid == 0) {      
-        printf("i am child process, pid : %d, ppid : %d\n", getpid(),getppid()); 
         sleep(1);   //等父进程执行完
         // 当前是子进程
-        printf("i am child process, pid : %d, ppid : %d\n", getpid(),getppid());
+        printf("I am child process, pid : %d, ppid : %d\n", getpid(),getppid());
 
     }
 
     // for循环
     for(int i = 0; i < 3; i++) {
-        printf("i : %d , pid : %d\n", i , getpid());
+        printf("I : %d , pid : %d\n", i , getpid());
     }
-
     return 0;
 }
 
 /*
-读时共享，写时拷贝
-实际上，更准确来说，Linux 的 fork() 使用是通过写时拷贝 (copy- on-write) 实现。
-写时拷贝是一种可以推迟甚至避免拷贝数据的技术。
-内核此时并不复制整个进程的地址空间，而是让父子进程共享同一个地址空间。
-只用在需要写入的时候才会复制地址空间，从而使各个进程拥有各自的地址空间。
-也就是说，资源的复制是在需要写入的时候才会进行，在此之前，只有以只读方式共享。
-注意：fork之后父子进程共享文件，
-fork产生的子进程与父进程相同的文件文件描述符指向相同的文件表，引用计数增加，共享文件偏移指针。
+    读时共享，写时拷贝
+    实际上，更准确来说，Linux 的 fork() 使用是通过写时拷贝 (copy- on-write) 实现。
+    写时拷贝是一种可以推迟甚至避免拷贝数据的技术。
+    内核此时并不复制整个进程的地址空间，而是让父子进程共享同一个地址空间。
+    只用在需要写入的时候才会复制地址空间，从而使各个进程拥有各自的地址空间。
+    也就是说，资源的复制是在需要写入的时候才会进行，在此之前，只有以只读方式共享。
+    注意：fork之后父子进程共享文件，
+    fork产生的子进程与父进程相同的文件文件描述符指向相同的文件表，
+    引用计数增加，共享文件偏移指针。
 */
