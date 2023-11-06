e3b84ac6712b19063756a06dcad5aa07c41e512f
Pan <494654334@qq.com>
2023-11-06 20:45:36 +0800

    解析http请求需要的函数以及状态量_状态机模型

--- webserver/http_conn.cpp
+++ webserver/http_conn.cpp
@@ -1,5 +1,4 @@
 #include "http_conn.h"
-
 //对静态变量初始化
 int http_conn::m_epollfd = -1;  
 int http_conn::m_users_count = 0;
@@ -16,7 +15,7 @@ int setnonblocking( int fd ) {
 void addfd( int epollfd, int fd, bool one_shot ) {
     epoll_event event;
     event.data.fd = fd;
-    event.events = EPOLLIN | EPOLLRDHUP;
+    event.events = EPOLLIN | EPOLLET |EPOLLRDHUP;
     if(one_shot) 
     {
         // 防止同一个通信被不同的线程处理
@@ -37,7 +36,7 @@ void removefd( int epollfd, int fd ) {
 void modfd(int epollfd, int fd, int ev) {
     epoll_event event;
     event.data.fd = fd;
-    event.events = ev | EPOLLET | EPOLLONESHOT | EPOLLRDHUP;
+    event.events = ev |EPOLLONESHOT | EPOLLRDHUP;
     epoll_ctl( epollfd, EPOLL_CTL_MOD, fd, &event );
 }
 
@@ -66,7 +65,28 @@ void http_conn::init(int sockfd, const sockaddr_in& addr){
 
 // 循环读取客户数据，直到无数据可读或者对方关闭连接
 bool http_conn::read() {
-    printf("read ing\n");
+    //特殊情况之->缓冲区大小不够接受客户端数据
+    if(m_read_idx >= READ_BUFFER_SIZE){
+        return false;
+    }
+    int bytes_read = 0;
+    while(1){
+        bytes_read = recv(m_sockfd,m_read_buf+m_read_idx,READ_BUFFER_SIZE-m_read_idx,0);
+        if(bytes_read == -1) {
+            if(errno == EAGAIN || errno == EWOULDBLOCK ){
+                //没有数据
+                break;
+            }
+            return false;
+        } else if(bytes_read == 0) {    //对方关闭连接或者读到末尾
+            // removefd(m_epollfd,m_sockfd);
+            // close_conn();  
+            return false;
+        } else if(bytes_read > 0) {
+            m_read_idx += bytes_read;
+        }
+    }
+    printf("读取到了数据:\n%s\n",m_read_buf);
     return true;
 }
 
@@ -77,8 +97,50 @@ bool http_conn::write()
     return true;
 }
 
-// 由线程池中的工作线程调用，这是处理HTTP请求的入口函数
+// 由线程池中的工作线程调用，这是解析HTTP请求的入口函数
 void http_conn::process() {
-    // 解析HTTP请求
+    //解析HTTP请求——使用有限状态机模型
+    HTTP_CODE read_ret = process_read();
+    if(read_ret == NO_REQUEST){
+        //请求不完整，需要继续读取客户数据
+        modfd(m_epollfd,m_sockfd,EPOLLIN);  //修改监听事件，并重新注册oneshot
+        return;
+    }else if(read_ret == GET_REQUEST){
+        //表示获得了一个完整的客户请求
+    }else if(read_ret == BAD_REQUEST){
+        //客户端请求语法错误
+    }
     printf("parse request，create response\n");
+}
+//主状态机
+http_conn::HTTP_CODE http_conn::process_read(){
+    LINE_STATUS line_status = LINE_OK;
+    while((line_status = parse_line()) == LINE_OK){
+        switch (expression)
+        {
+        case /* constant-expression */:
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
+    }
+    return GET_REQUEST;
+}
+//解析http请求中的“行”
+http_conn::LINE_STATUS http_conn::parse_line(){
+    return LINE_OK;
+}
+//解析http请求首行
+http_conn::HTTP_CODE http_conn::parse_request_line(char* text){
+    return GET_REQUEST;
+}
+//解析http请求头
+http_conn::HTTP_CODE http_conn::parse_headers(char* text){
+    return GET_REQUEST;
+}
+//解析http请求体
+http_conn::HTTP_CODE http_conn::parse_body(char* text){
+    return GET_REQUEST;
 }
--- webserver/http_conn.h
+++ webserver/http_conn.h
@@ -27,23 +27,56 @@ public:
     static const int FILENAME_LEN = 200;        // 文件名的最大长度
     static const int READ_BUFFER_SIZE = 2048;   // 读缓冲区的大小
     static const int WRITE_BUFFER_SIZE = 1024;  // 写缓冲区的大小
+    static int m_epollfd;       // 所有socket上的事件都被注册到同一个epoll内核事件中，所以设置成静态的
+    static int m_users_count;    // 统计用户的数量
+    // HTTP请求方法，这里只支持GET
+    enum METHOD {GET = 0, POST, HEAD, PUT, DELETE, TRACE, OPTIONS, CONNECT};
+    
+    /*
+        解析客户端请求时，主状态机的状态
+        CHECK_STATE_REQUESTLINE:当前正在分析请求行
+        CHECK_STATE_HEADER:当前正在分析头部字段
+        CHECK_STATE_CONTENT:当前正在解析请求体
+    */
+    enum CHECK_STATE { CHECK_STATE_REQUESTLINE = 0, CHECK_STATE_HEADER, CHECK_STATE_CONTENT };
+    
+    /*
+        服务器处理HTTP请求的可能结果，报文解析的结果
+        NO_REQUEST          :   请求不完整，需要继续读取客户数据
+        GET_REQUEST         :   表示获得了一个完整的客户请求
+        BAD_REQUEST         :   表示客户请求语法错误
+        NO_RESOURCE         :   表示服务器没有资源
+        FORBIDDEN_REQUEST   :   表示客户对资源没有足够的访问权限
+        FILE_REQUEST        :   文件请求,获取文件成功
+        INTERNAL_ERROR      :   表示服务器内部错误
+        CLOSED_CONNECTION   :   表示客户端已经关闭连接了
+    */
+    enum HTTP_CODE { NO_REQUEST, GET_REQUEST, BAD_REQUEST, NO_RESOURCE, FORBIDDEN_REQUEST, FILE_REQUEST, INTERNAL_ERROR, CLOSED_CONNECTION };
+    
+    // 从状态机的三种可能状态，即行的读取状态，分别表示
+    // 1.读取到一个完整的行 2.行出错 3.行数据尚且不完整
+    enum LINE_STATUS { LINE_OK = 0, LINE_BAD, LINE_OPEN };
 public:
     http_conn(){}
     ~http_conn(){}
-public:
     void init(int sockfd, const sockaddr_in& addr); // 初始化新接受的连接
     void close_conn();  // 关闭连接
     void process(); // 处理客户端请求
     bool read();// 非阻塞读
     bool write();// 非阻塞写
-public:
-    static int m_epollfd;       // 所有socket上的事件都被注册到同一个epoll内核事件中，所以设置成静态的
-    static int m_users_count;    // 统计用户的数量
 private:
-    int m_sockfd;           // 该HTTP连接的socket和对方的socket地址
-    sockaddr_in m_address;
+    int m_sockfd;           //该HTTP连接的socket和对方的socket地址
+    sockaddr_in m_address;  //socket地址
+    char m_read_buf[READ_BUFFER_SIZE];    //读缓冲区
+    int m_read_idx; //标识读缓冲区中以及读入的客户端数据的最后一个字节的下一个位置
+    char m_write_buf[WRITE_BUFFER_SIZE];    //读缓冲区
 private:
     void init();    // 初始化连接
+    HTTP_CODE process_read();// 解析HTTP请求
+    HTTP_CODE parse_request_line(char* text);// 解析请求首行
+    HTTP_CODE parse_headers(char* text);// 解析请求头
+    HTTP_CODE parse_body(char* text);// 解析请求体
+    LINE_STATUS parse_line();//解析一行
 };
 
 #endif
